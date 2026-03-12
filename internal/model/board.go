b67364ffa5dc0bb36206830491aa21c27c4554f5
nbougial <nik_mpougialas@hotmail.com>
2026-03-12 17:56:29 +0200

    Add board helper methods for task 14

--- internal/model/board.go
+++ internal/model/board.go
@@ -1,5 +1,7 @@
 package model
 
+import "strings"
+
 const EmptyCell = '.'
 
 type Board struct {
@@ -21,4 +23,40 @@ func NewBoard(size int) Board {
 	}
 
 	return board
+}
+
+func (b Board) InBounds(row, col int) bool {
+	return row >= 0 && row < b.Size && col >= 0 && col < b.Size
+}
+
+func (b Board) Get(row, col int) rune {
+	return b.Cells[row][col]
+}
+
+func (b *Board) Set(row, col int, value rune) {
+	b.Cells[row][col] = value
+}
+
+func (b Board) Clone() Board {
+	cloned := Board{
+		Size:  b.Size,
+		Cells: make([][]rune, b.Size),
+	}
+
+	for row := 0; row < b.Size; row++ {
+		cloned.Cells[row] = make([]rune, b.Size)
+		copy(cloned.Cells[row], b.Cells[row])
+	}
+
+	return cloned
+}
+
+func (b Board) String() string {
+	rows := make([]string, b.Size)
+
+	for row := 0; row < b.Size; row++ {
+		rows[row] = string(b.Cells[row])
+	}
+
+	return strings.Join(rows, "\n")
 }
